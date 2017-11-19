ec37d8e0905ed60240dc2e5397fa69e6d1f5ea20
Firdows <ahamad.jedu@gmail.com>
2017-11-19 04:08:57 +0000

    remove water pager

--- backend/config/modules/pdfjs.php
+++ backend/config/modules/pdfjs.php
@@ -2,7 +2,7 @@
 return  [
         'class' => 'yii2assets\pdfjs\Module',
         'waterMark'=>[
-           'text'=>'อบต.ยะลา',
+           //'text'=>'อบต.ยะลา',
            'color'=> '#999',
            'alpha'=>'0.3'
          ]
