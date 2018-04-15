697a06c1ca19099ed601c5900335c672fd406eb5
Ahamad-PC <ahamad.jedu@gmail.com>
2018-04-15 15:20:08 +0700

    bug timeago

--- backend/views/layouts/_leave.php
+++ backend/views/layouts/_leave.php
@@ -1,7 +1,9 @@
 <?php
+
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\helpers\ArrayHelper;
+use anda\timeago\TimeAgo;
 use andahrm\leave\models\LeaveSearch;
 use andahrm\leave\models\LeaveCommanderSearch;
 use andahrm\leave\models\LeaveCommanderCancelSearch;
@@ -9,7 +11,7 @@ use andahrm\leave\models\LeaveInspactorSearch;
 use andahrm\leave\models\LeaveDirectorSearch;
 use andahrm\leave\models\LeaveDirectorCancelSearch;
 
-$total=0;
+$total = 0;
 
 $searchModel = new LeaveCommanderSearch();
 $dataCommander = $searchModel->search(Yii::$app->request->queryParams);
@@ -22,75 +24,63 @@ $dataInspactor = $searchModel->search(Yii::$app->request->queryParams);
 
 $searchModel = new LeaveDirectorSearch();
 $dataDirector = $searchModel->search(Yii::$app->request->queryParams);
-        
+
 $searchModel = new LeaveDirectorCancelSearch();
 $dataDirectorCancel = $searchModel->search(Yii::$app->request->queryParams);
 
 
-$total = $dataCommander->getCount() 
-    + $dataCommanderCancel->getCount()
-    + $dataInspactor->getCount()
-    + $dataDirector->getCount() 
-    + $dataDirectorCancel->getCount();
+$total = $dataCommander->getCount() + $dataCommanderCancel->getCount() + $dataInspactor->getCount() + $dataDirector->getCount() + $dataDirectorCancel->getCount();
 
 $dataModels = ArrayHelper::merge(
-    ['/leave/commander/consider'=>$dataCommander->getModels()],
-    ['/leave/commander/consider-cancel'=>$dataCommanderCancel->getModels()],
-    ['/leave/inspector/consider'=>$dataInspactor->getModels()],
-    ['/leave/director/consider'=>$dataDirector->getModels()],
-    ['/leave/director/consider-cancel'=>$dataDirectorCancel->getModels()]
+                ['/leave/commander/consider' => $dataCommander->getModels()], ['/leave/commander/consider-cancel' => $dataCommanderCancel->getModels()], ['/leave/inspector/consider' => $dataInspactor->getModels()], ['/leave/director/consider' => $dataDirector->getModels()], ['/leave/director/consider-cancel' => $dataDirectorCancel->getModels()]
 );
-    
-    //print_r($models);
 
-
+//print_r($models);
 ?>
 
 <li role="presentation" class="dropdown">
-        <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
-            <i class="fa fa-calendar-check-o"></i>
-            <?php if($total):?>
-            <span class="badge bg-red"><?=$total?></span>
-            <?php endif;?>
-        </a>
-        
-        
-        
+    <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
+        <i class="fa fa-calendar-check-o"></i>
+        <?php if ($total): ?>
+            <span class="badge bg-red"><?= $total ?></span>
+        <?php endif; ?>
+    </a>
+
+
+
     <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
-        
+
         <?php
-        if($dataModels):
-        foreach($dataModels as $link => $models):
-            foreach($models as $model):?>
-            <li>
-                <a href="<?=Url::to([$link,'id'=>$model->id])?>">
-                    <span class="image">
-                        <?=Html::img($model->createdBy->photoLast)?>
-                    </span>
-                    <span>
-                        <span><?=$model->createdBy->fullname?></span>
-                        <span class="time"><?= \anda\timeago\TimeAgo::widget(['timestamp' => $model->updated_at])?></span>
-                    </span>   
-                    <span class="message">
-                        พิจารณา<?=$model->leaveType->title;?>
-                    </span>
-                    
-                </a>
-            </li>
-            <?php
+        if ($dataModels):
+            foreach ($dataModels as $link => $models):
+                foreach ($models as $model):
+                    ?>
+                    <li>
+                        <a href="<?= Url::to([$link, 'id' => $model->id]) ?>">
+                            <span class="image">
+                                <?= Html::img($model->createdBy->photoLast) ?>
+                            </span>
+                            <span>
+                                <span><?= $model->createdBy->fullname ?></span>
+                                <span class="time">
+                                    <?php echo TimeAgo::widget(['timestamp' => $model->updated_at]); ?>
+                                </span>
+                            </span>   
+                            <span class="message">
+                                พิจารณา<?= $model->leaveType->title; ?>
+                            </span>
+
+                        </a>
+                    </li>
+                    <?php
+                endforeach;
             endforeach;
-        endforeach;
         endif;
-        
-        
-        
-        
-       
         ?>
         <li>
             <div class="text-center">
-                 <a href="<?=Url::to(['/leave/'])?>">
-                    <strong><?=Yii::t('andahrm','See All ');?></strong>
+                <a href="<?= Url::to(['/leave/']) ?>">
+                    <strong><?= Yii::t('andahrm', 'See All '); ?></strong>
                     <i class="fa fa-angle-right"></i>
                 </a>
             </div>
